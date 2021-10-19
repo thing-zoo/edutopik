b723178040e929f48213c76ef49539aa19903390
thing-zoo <cathy4025@gmail.com>
2021-10-19 23:07:25 +0900

    update video data

--- lib/models/mock_data.dart
+++ lib/models/mock_data.dart
@@ -4,25 +4,25 @@ Map<String, dynamic> mockData = {
       "title": "Rio from Above",
       "image": "images/rio_from_above_poster.jpg",
       "trailer_url":
-          "https://v.ftcdn.net/04/49/11/04/700_F_449110446_C5BqhqglYI491J9K27f0pKbReOUYUzWT_ST.mp4",
+          "https://github.com/GeekyAnts/flick-video-player-demo-videos/blob/master/example/rio_from_above_compressed.mp4?raw=true",
     },
     {
       "title": "The Valley",
       "image": "images/the_valley_poster.jpg",
       "trailer_url":
-          "https://v.ftcdn.net/04/49/13/89/700_F_449138937_FILTKuqTqKZgh4SKQ76bFKyi6cFfbXPC_ST.mp4",
+          "https://github.com/GeekyAnts/flick-video-player-demo-videos/blob/master/example/the_valley_compressed.mp4?raw=true",
     },
     {
       "title": "Iceland",
       "image": "images/iceland_poster.jpg",
       "trailer_url":
-          "https://v.ftcdn.net/04/52/64/06/700_F_452640633_pxPwAX08y6KkeWoDjDrhvBdldG3lEm5u_ST.mp4",
+          "https://github.com/GeekyAnts/flick-video-player-demo-videos/blob/master/example/iceland_compressed.mp4?raw=true",
     },
     {
       "title": "9th May & Fireworks",
       "image": "images/9th_may_poster.jpg",
       "trailer_url":
-          "https://v.ftcdn.net/04/51/30/85/700_F_451308584_wzAClQZWkrXBH3iIcImudaxNMPYLZCIq_ST.mp4",
+          "https://github.com/GeekyAnts/flick-video-player-demo-videos/blob/master/example/9th_may_compressed.mp4?raw=true",
     },
   ]
 };
