8509cd54bdc4329c6d6514957152a3038e39b436
thing-zoo <cathy4025@gmail.com>
2021-12-02 19:18:02 +0900

    update

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -30,9 +30,6 @@ class Init {
   static final instance = Init._();
 
   Future initialize() async {
-    // This is where you can initialize the resources needed by your app while
-    // the splash screen is displayed.  Remove the following example because
-    // delaying the user experience is a bad design practice!
     await Future.delayed(const Duration(seconds: 3));
   }
 }
